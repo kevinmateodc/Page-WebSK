86181c588efcd6126de5d5a96f2a5c579d271f56
kevinmateodc <128754401+kevinmateodc@users.noreply.github.com>
2023-06-14 16:58:59 -0500

    proyect archived
    it is the proyect for representation for evaluation end

---
+++ datos.php
@@ -0,0 +1,17 @@
+<?php
+     //conexion con la base de datos y el servidor 
+     $link=mysql connected("localhost", "root","")  
+     $db= mysql_select_db("usuarios", $link) 
+
+    //obtenemos los valores del formulario 
+    $nombre =$_POST['']
+    $correo =$POST
+    $telefono =$POST
+    $producto =$POST
+
+    //obtiene la longitud de un string 
+    $req = (strlen ($nombre)*trlen ($correo))*strlen($telefono)*strlen ($producto)
+
+
+    //ingresar informacion a la base de datos
+    mysql_admin("INSERT INTO datos VALUES ('', 'precio', 'cantidad', 'empresa')",$link)
